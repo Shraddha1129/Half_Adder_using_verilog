69d6c35ad3bdc3ade259443cb01934fd7ccef79c
Shraddha1129 <kshraddha254@gmail.com>
2025-03-01 12:15:32 +0530

    Add files via upload

---
+++ Testbench.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+
+module Testbench_v;
+
+	// Inputs
+	reg A;
+	reg B;
+
+	// Outputs
+	wire Sum;
+	wire Carry;
+
+	// Instantiate the Unit Under Test (UUT)
+	Half_Adder uut (
+		.A(A), 
+		.B(B), 
+		.Sum(Sum), 
+		.Carry(Carry)
+	);
+
+	initial begin
+		// Apply test cases
+        A = 0; B = 0; #10;  // Test case 1
+        A = 0; B = 1; #10;  // Test case 2
+        A = 1; B = 0; #10;  // Test case 3
+        A = 1; B = 1; #10;  // Test case 4
+		  
+		  #10;
+		  $display("Test completed");
+        $finish;
+	end
+      
+endmodule
+

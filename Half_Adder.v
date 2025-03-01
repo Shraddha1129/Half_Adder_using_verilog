e16a335f4423754fca8c01a9d22c309a31ba334c
Shraddha1129 <kshraddha254@gmail.com>
2025-03-01 12:19:47 +0530

    Add files via upload

---
+++ Half_Adder.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+
+module Half_Adder(A, B, Sum, Carry);
+    input A;
+    input B;
+    output Sum;
+    output Carry;
+	 
+	 assign Sum = A^B; //XOR for sum
+	 assign Carry = A&B; // AND for carry
+	 
+
+
+endmodule
